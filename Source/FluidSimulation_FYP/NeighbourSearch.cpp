d59d29d98b450e96edfa0f2f1df33267c489209f
Rodrigo Roldan Romero <luisrodrigoroldan@hotmail.com>
2022-02-10 18:24:41 +0000

    Implemented first version of neighbour search using spatial hashing
    there might be some issues with the direction axis.
    I might change the algorithm if I find a faster one.
    The neighbour search still needs to be linked to the solver, iterate and cache the nearby particles.

--- Source/FluidSimulation_FYP/NeighbourSearch.cpp
+++ Source/FluidSimulation_FYP/NeighbourSearch.cpp
@@ -4,9 +4,103 @@
 #include "NeighbourSearch.h"
 #include "FluidParticle.h"
 
-size_t UNeighbourSearch::getHashKeyFromPosition(const FVector& pos)
+size_t UNeighbourSearch::getHashKeyFromPosition(const FVector& pos) const
 {
-	return 0;
+	FIntVector bucketIndex;
+
+	//get the bucket index
+	bucketIndex = getBucketIndex(pos);
+
+	//get the hash key from the bucket index
+	return getHashKeyFromBucketIndex(bucketIndex);
+}
+
+FIntVector UNeighbourSearch::getBucketIndex(const FVector& pos) const
+{
+	FIntVector bucketIndex;
+	//need to round these values down
+	bucketIndex.X = pos.X / m_gridSpacing;
+	bucketIndex.Y = pos.Y / m_gridSpacing;
+	bucketIndex.Z = pos.Z / m_gridSpacing;
+	return bucketIndex;
+}
+
+size_t UNeighbourSearch::getHashKeyFromBucketIndex(const FIntVector& bucketIndex) const
+{
+	FIntVector wrappedIndex = bucketIndex;
+	wrappedIndex.X = bucketIndex.X % m_resolution.X;
+	wrappedIndex.Y = bucketIndex.Y % m_resolution.Y;
+	wrappedIndex.Z = bucketIndex.Z % m_resolution.Z;
+
+	if (wrappedIndex.X < 0) { wrappedIndex.X += m_resolution.X; }
+	if (wrappedIndex.Y < 0) { wrappedIndex.Y += m_resolution.Y; }
+	if (wrappedIndex.Z < 0) { wrappedIndex.Z += m_resolution.Z; }
+
+	//there might be some issues with the way Unreal has its direction axis.
+	return static_cast<size_t>((wrappedIndex.Z * m_resolution.Y + wrappedIndex.Y) * m_resolution.X + wrappedIndex.X);
+}
+
+void UNeighbourSearch::getNearbyKeys(const FVector& pos, size_t* nearbyKeys) const
+{
+	FIntVector originIndex = getBucketIndex(pos);
+	FIntVector nearbyBucketIndices[8];
+
+	for (int i = 0; i < 8; i++)
+	{
+		nearbyBucketIndices[i] = originIndex;
+	}
+
+	if ((originIndex.X + 0.5f) * m_gridSpacing <= pos.X)
+	{
+		nearbyBucketIndices[4].X += 1;
+		nearbyBucketIndices[5].X += 1;
+		nearbyBucketIndices[6].X += 1;
+		nearbyBucketIndices[7].X += 1;
+	}
+	else
+	{
+		nearbyBucketIndices[4].X -= 1;
+		nearbyBucketIndices[5].X -= 1;
+		nearbyBucketIndices[6].X -= 1;
+		nearbyBucketIndices[7].X -= 1;
+	}
+
+	if ((originIndex.Y + 0.5f) * m_gridSpacing <= pos.Y)
+	{
+		nearbyBucketIndices[2].Y += 1;
+		nearbyBucketIndices[3].Y += 1;
+		nearbyBucketIndices[6].Y += 1;
+		nearbyBucketIndices[7].Y += 1;
+	}
+	else
+	{
+		nearbyBucketIndices[2].Y -= 1;
+		nearbyBucketIndices[3].Y -= 1;
+		nearbyBucketIndices[6].Y -= 1;
+		nearbyBucketIndices[7].Y -= 1;
+	}
+
+	if ((originIndex.Z + 0.5f) * m_gridSpacing <= pos.Z)
+	{
+		nearbyBucketIndices[1].Z += 1;
+		nearbyBucketIndices[3].Z += 1;
+		nearbyBucketIndices[5].Z += 1;
+		nearbyBucketIndices[7].Z += 1;
+	}
+	else
+	{
+		nearbyBucketIndices[1].Z -= 1;
+		nearbyBucketIndices[3].Z -= 1;
+		nearbyBucketIndices[5].Z -= 1;
+		nearbyBucketIndices[7].Z -= 1;
+	}
+
+	//this might be wrong due to Unreal's change of direction axis.
+
+	for (int i = 0; i < 8; i++)
+	{
+		nearbyKeys[i] = getHashKeyFromBucketIndex(nearbyBucketIndices[i]);
+	}
 }
 
 // Sets default values for this component's properties
@@ -40,6 +134,36 @@ void UNeighbourSearch::build(const TArray<class AFluidParticle*>& points)
 		m_particlePositions[i] = points[i]->GetParticlePosition();
 		size_t key = getHashKeyFromPosition(m_particlePositions[i]);
 		m_buckets[key].Push(i);
+	}
+}
+
+void UNeighbourSearch::forEachNearbyPoint(const FVector& origin, double radius)
+{
+	if (m_buckets.Num() == 0)
+	{
+		return;
+	}
+
+	size_t nearbyKeys[8];
+	getNearbyKeys(origin, nearbyKeys);
+
+	const double queryRadiusSquared = radius * radius;
+
+	for (int i = 0; i < 8; i++)
+	{
+		const auto& bucket = m_buckets[nearbyKeys[i]];
+		size_t numberOfPointsInBucket = bucket.Num();
+
+		for (size_t j = 0; j < numberOfPointsInBucket; ++j)
+		{
+			size_t pointIndex = bucket[j];
+			double rSquared = (m_particlePositions[pointIndex] - origin).SizeSquared();
+			if (rSquared <= queryRadiusSquared)
+			{
+				//callback
+
+			}
+		}
 	}
 }
 
--- Source/FluidSimulation_FYP/NeighbourSearch.h
+++ Source/FluidSimulation_FYP/NeighbourSearch.h
@@ -13,11 +13,14 @@ class FLUIDSIMULATION_FYP_API UNeighbourSearch : public UActorComponent
 	GENERATED_BODY()
 
 	double m_gridSpacing = 1.0f;
-	FVector m_resolution = FVector(1);
+	FIntVector m_resolution = FIntVector(1, 1, 1);
 	TArray<TArray<size_t>> m_buckets;
 	TArray<FVector> m_particlePositions;
 
-	size_t getHashKeyFromPosition(const FVector& pos);
+	size_t getHashKeyFromPosition(const FVector& pos) const;
+	FIntVector getBucketIndex(const FVector& pos) const;
+	size_t getHashKeyFromBucketIndex(const FIntVector& bucketIndex) const;
+	void getNearbyKeys(const FVector& pos, size_t* nearbyKeys) const;
 
 public:	
 	// Sets default values for this component's properties
